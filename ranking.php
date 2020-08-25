740ab55b84ade8c3789410c7d0a4416467ac57ad
Wa59 <wesleywa59@gmail.com>
2020-08-25 13:08:42 -0300

    Logos

--- ranking.php
+++ ranking.php
@@ -1,9 +1,16 @@
 <?php 
-$csv = array_map('str_getcsv', file('top20-100.csv'));
-array_walk($csv, function(&$a) use ($csv) {
-  $a = array_combine($csv[0], $a);
+$top20_100 = array_map('str_getcsv', file('top20-100.csv'));
+$top_categoria = array_map('str_getcsv', file('top-categoria.csv'));
+
+array_walk($top20_100, function(&$a) use ($top20_100) {
+  $a = array_combine($top20_100[0], $a);
 });
-array_shift($csv);
+array_shift($top20_100);
+
+array_walk($top_categoria, function(&$a) use ($top_categoria) {
+  $a = array_combine($top_categoria[0], $a);
+});
+array_shift($top_categoria);
 ?>
 <?php require ('./inc/header.php'); ?>
 
@@ -35,7 +42,34 @@ array_shift($csv);
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($csv as $k => $value) { 
+                <?php foreach ($top20_100 as $k => $value) { 
+                    if ($value['Ranking'] != 'A-Z') {?>
+                        <tr>
+                            <td><span><?php echo $value['Ranking']; ?></span></td>
+                            <td>
+                                <img class="img-fluid" src="/assets/images/logos/<?php echo $value['Empresa']; ?>.png" alt="Logotipo <?php echo $value['Empresa']; ?>">
+                                <?php echo $value['Empresa']; ?>
+                            </td>
+                            <td><span><?php echo $value['Pontos']; ?></span></td>
+                            <td><span><a href="<?php echo $value['Link']; ?>" class="btn">Quero me conectar</a></span></td>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+            </tbody>
+        </table>
+
+        <h2>TOP 1 CATEGORIAS OPEN CORPS</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th width="01%"><span>Rank</span></th>
+                    <th width="93%"><span>Empresa</span></th>
+                    <th width="01%"><span>Pontos</span></th>
+                    <th width="05%"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($top20_100 as $k => $value) { 
                     if ($value['Ranking'] != 'A-Z') {?>
                         <tr>
                             <td><span><?php echo $value['Ranking']; ?></span></td>
@@ -50,6 +84,28 @@ array_shift($csv);
                 <?php } ?>
             </tbody>
         </table>
+
+        <h2>TOP 100 OPEN CORPS</h2>
+
+        <div class="flex-container">
+            <?php foreach ($top20_100 as $k => $value) { 
+                if ($value['Ranking'] == 'A-Z') {?>
+
+
+                    <?php echo $value['Empresa']; ?><br>
+
+                    <?php /* // ?>
+                    <div class="box">
+                        <figure>
+                            <img class="img-fluid" src="/assets/images/logos/<?php echo $value['Empresa']; ?>.png" alt="Logotipo <?php echo $value['Empresa']; ?>">
+                        </figure>
+                        <a href="<?php echo $value['Link']; ?>" class="btn">Quero me conectar</a>
+                    </div>
+                    <?php // */ ?>
+                <?php } ?>
+            <?php } ?>
+        </div>
+
 
     </div><!--/.container -->
     </div><!--/.page__body -->
