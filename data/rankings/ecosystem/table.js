4e8791100646c6d5c51c20f4358a9f73e25d87a9
William Daflita <william.daflita@gmail.com>
2020-08-28 06:48:34 -0300

    RKN Ecosystem e Eval

---
+++ data/rankings/ecosystem/table.js
@@ -0,0 +1,35 @@
+var cYear = 2019;
+var years = {
+    2019: topEcosystem2019,
+};
+
+$(document).ready(function($) {
+
+    function load() {
+        var html = '';
+
+        if (!years[cYear]) {
+            return false;
+        }
+
+        years[cYear].forEach(function(item, index){
+            // Include line on the table
+            html+= '<tr>';
+                html+= '<td class="text-glow">' + item['rank'] + '</td>';
+                html+= '<td class="text-left">' + item['name'] + '</td>';
+                html+= '<td>' + item['points'] + '</td>';
+            html+= '</tr>';
+        });
+        $('#tableBody').html(html);
+        $('#name').change();
+    }
+
+    load();
+
+    $('#year').on('change', function(ev) {
+        var value = ev.target.value;
+        cYear = value;
+        load();
+    });
+
+}); // document.ready
